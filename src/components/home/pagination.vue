53d4e6cf394a6dd14596967dc0a5f723bdce52f5
wander <junjiehuang@yeah.net>
2018-04-25 23:07:21 +0800

    首页支持分页 自写分页栏 引入driver.js作为提示框

---
+++ src/components/home/pagination.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="pagination">
+    <div class="page-pre" @click="directPre"></div>
+    <div class="page-current">{{page}}</div>
+    <div class="page-after" @click="directAfter"></div>
+  </div>
+</template>
+<script>
+// driverjs 浮动窗口
+import Driver from 'driver.js'
+const driver = new Driver()
+export default {
+  name: 'pagination',
+  props: {
+    page: {
+      type: Number,
+      default: () => {
+        return 0
+      }
+    },
+    err: {
+      type: Boolean,
+      default: () => {
+        return false
+      }
+    }
+  },
+  watch: {
+    err (newVal, oldVal) {
+      if (newVal) {
+        driver.highlight({
+          element: '.pagination',
+          popover: {
+            title: '查询出错'
+          }
+        })
+      }
+    }
+  },
+  methods: {
+    directPre () {
+      if (this.page <= 1) {
+        driver.highlight({
+          element: '.page-pre',
+          popover: {
+            title: '已经是第一页拉！'
+          }
+        })
+      } else {
+        this.$emit('queryPage', this.page - 1)
+      }
+    },
+    directAfter (err) {
+      if (err === false) {
+        driver.highlight({
+          element: '.page-after',
+          popover: {
+            title: '已经是最后一页拉！'
+          }
+        })
+      } else {
+        this.$emit('queryPage', this.page + 1)
+      }
+    }
+  }
+}
+</script>
+<style>
+/*引入driver.js 默认样式 lib中做了自定义修改*/
+@import "../../../static/css-lib/driver.min.css"
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,43 +1,65 @@
 <template>
   <section class="home-page">
     <articlePreview v-for="(info, index) in news" :view ="info" :key="info.title + index"></articlePreview>
+    <pagination :page="page" @queryPage="queryPage" ref="pagination"></pagination>
   </section>
 </template>
 <script>
 import articlePreview from '@/components/home/Preview'
+import pagination from '@/components/home/pagination'
 import {backend} from '../../config'
 const URL_ARTICLES = backend.base + backend.home + '/'
 export default {
   data () {
     return {
-      news: []
+      news: [],
+      page: 0,
+      err: false
     }
   },
   components: {
-    articlePreview
+    articlePreview, pagination
   },
   created () {
-    // TODO 封装axios 制作分页栏
-    let $this = this
-    this.$ajax
-      .get('/queryAll', {
-        baseURL: URL_ARTICLES + 'articles',
-        params: {
-          page: 1
-        },
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest',
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(function (result) {
-        console.log(result)
-        $this.news = result.data[0].items
-      })
-      .catch(function (err) {
-        console.log(err)
-      })
+    this.requestPage(1)
+  },
+  methods: {
+    requestPage (page) {
+      // TODO 封装axios 制作分页栏
+      let $this = this
+      this.$ajax
+        .get('/queryAll', {
+          baseURL: URL_ARTICLES + 'articles',
+          params: {
+            page: page
+          },
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(function (result) {
+          console.log('receive result: ' + result)
+          try {
+            $this.news = result.data[0].items
+            $this.page = result.data[0].page
+          } catch (err) {
+            $this.err = true
+            $this.$refs.pagination.directAfter(false)
+            console.log(result)
+          }
+        })
+        .catch(function (err) {
+          $this.err = true
+          $this.$refs.pagination.directAfter(false)
+          console.log(err)
+        })
+    },
+    queryPage (page) {
+      this.requestPage(page)
+    }
   }
+
 }
 </script>
 <style scoped>
